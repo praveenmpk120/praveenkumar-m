78f2ddc27df760ba1d3fe9deeae98a4d7f7b8fd9
praveenmpk120 <35652854+praveenmpk120@users.noreply.github.com>
2018-01-30 16:17:56 +0530

    Create mpk1.c

---
+++ mpk1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<coni.h>
+void main()
+{
+char n;
+clrscr();
+printf("enter a character");
+scanf("%c",&n);
+if(n==a&&n==e&&n==i&&n==o&&u)
+{
+printf("Alfapht");
+}
+else
+{
+printf("no");
+}
+getch();
+}
