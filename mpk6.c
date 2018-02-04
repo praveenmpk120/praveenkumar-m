43a2573abbd527cb307243597d966d3d4ce3a6ee
praveenmpk120 <35652854+praveenmpk120@users.noreply.github.com>
2018-02-04 09:23:34 +0530

    Create mpk6.c

---
+++ mpk6.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,k,i,j;
+clrscr();
+printf("enter a value of n,k");
+scanf("%d,%d",&n,&k);
+for(i=1;i<=n;i++)
+{
+printf("%d",n);
+}
+fof(j=1;j<=k;j++)
+{
+sum=sum+j;
+}
+printf("%d",sum);
+return0;
+getch();
+}
