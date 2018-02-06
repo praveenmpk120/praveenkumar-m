b725896418026802326415196501b491190d73cd
praveenmpk120 <35652854+praveenmpk120@users.noreply.github.com>
2018-02-06 16:57:12 +0530

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i,s;
+    printf("enter n value");
+    scanf("%d",&n);
+    for(i=2;i<=n/2;++i)
+    {
+        if(n%i=0)
+        {
+           if(s=0)
+           {
+            printf("the number is prime");
+        }
+        else
+        {
+            
+            printf("the number is not prime");
+    
+        }
+    }
+    getch();
+}
+}
