7356f6a8cff9e5b7a00173b333f9597f5aaf60ea
praveenmpk120 <35652854+praveenmpk120@users.noreply.github.com>
2018-02-08 15:55:31 +0530

    Create 15.c

---
+++ 15.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,n;
+    clrscr();
+    printf("enter n value");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        if(i%2==0)
+        {
+            printf("%d",i);
+        }
+    }
+    getch();
+}
+
+
