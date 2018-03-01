403c25d429e6d19bd9144f94e8c6958d9c8cc03d
praveenmpk120 <35652854+praveenmpk120@users.noreply.github.com>
2018-03-01 19:22:54 +0800

    Create 31.c

---
+++ 31.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+void main ()
+{
+    char a[50];
+    clrscr();
+    printf("enter character");
+    scanf("%s",a);
+    len=strlen[a];
+    printf("string lenth is =%d",len);
+    getch();
+}
