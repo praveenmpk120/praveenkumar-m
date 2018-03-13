4ee1e598d593cec2c7728dd8f92d433a228aadb7
praveenmpk120 <35652854+praveenmpk120@users.noreply.github.com>
2018-03-13 18:39:45 +0800

    Create 32.c

---
+++ 32.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    char a[50];
+    int i,c=0;
+    clrscr();
+    printf("enter a sentence");
+    scanf("%[^\n]s",a);
+    for(i=0;a[i]!='\0';i++)
+    {
+    if(a[i]==' ')
+    {
+        c++;
+    }
+    }
+    printf("the sentence is =%d",c);
+    getch();
+}
