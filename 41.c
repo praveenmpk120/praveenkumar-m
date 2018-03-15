e778ff9045d02658393643aa7b4d4295accef330
praveenmpk120 <35652854+praveenmpk120@users.noreply.github.com>
2018-03-15 19:13:10 +0800

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a[50];
+    int n,i,b;
+    clrscr();
+    
+    printf("Enter a chracter name");
+    scanf("%s",a);
+    printf("enter the limits");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        
+printf("\n%s",a);
+            
+        }
+        getch();
+    }
