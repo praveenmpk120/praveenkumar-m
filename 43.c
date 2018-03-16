2513e60a6af95e4073605940f4589dedb626aa85
praveenmpk120 <35652854+praveenmpk120@users.noreply.github.com>
2018-03-16 16:54:57 +0800

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a[20],b[50];
+    clrscr();
+    printf("enter name1");
+    scanf("%s",a);
+    printf("enter name2");
+    scanf("%s",b);
+    printf("%s%s",a,b);
+getch();
+}
