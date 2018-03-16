9dcff2373ec62f197fb5efd3095cc4b24fe96536
praveenmpk120 <35652854+praveenmpk120@users.noreply.github.com>
2018-03-16 17:04:15 +0800

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,a;
+    clrscr();
+    printf("enter a va;lue");
+    scanf("%d",&a);
+    
+        if(a>=1&&a<=10)
+        {
+            printf("yes");
+            
+        }
+        else
+        {
+            printf("no");
+        }
+    
+    getch();
+    
+}
