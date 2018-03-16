b151ea7908241e1578723861de8ebcc79a73d7bd
praveenmpk120 <35652854+praveenmpk120@users.noreply.github.com>
2018-03-16 17:17:37 +0800

    Create 45.c

---
+++ 45.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,a[100],n=1;
+    clrscr();
+    printf("enter a va;lue");
+    scanf("%d",&a);
+    for(i=1;a[i]!='\0';i++)
+    
+        if(a>0)
+        {
+        n++;
+        
+        
+    
+        
+        }
+        
+            printf("%d=given input digit",n);
+    
+    getch();
+    
+}
