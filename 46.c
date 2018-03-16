f40c3a471d6320d3b8eaf6c1bd67f2b482922610
praveenmpk120 <35652854+praveenmpk120@users.noreply.github.com>
2018-03-16 17:22:51 +0800

    Create 46.c

---
+++ 46.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,n=0;
+    clrscr();
+    printf("enter a va;lue");
+    scanf("%d",&a);
+    n=a+1;
+    printf("%d=given input",n);
+    
+    getch();
+    
+}
