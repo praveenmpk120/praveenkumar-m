6be4962563f9b567585f0d663ce92d43a2f27f78
praveenmpk120 <35652854+praveenmpk120@users.noreply.github.com>
2018-03-20 19:17:12 +0800

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+    clrscr();
+    printf("enter a value");
+    scanf("%d",&a);
+if(a%2==0)
+{
+    printf("yes");
+}
+else
+{
+    printf("no");
+}
+getch();
+}
